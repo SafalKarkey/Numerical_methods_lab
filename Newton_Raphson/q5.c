5c35341e82bd6aea841c6a084f80513304bcbc08
Safal <sawfal24@gmail.com>
2022-12-21 23:14:48 +0545

    added q5 and q6

--- Newton_Raphson/q5.c
+++ Newton_Raphson/q5.c
@@ -2,14 +2,18 @@
 #include<math.h>
 #include<stdlib.h>
 
-#define f(x) 3*x-cos(x)-1
-#define df(x) 3+sin(x)
+float f(float x){
+    return 4*sin(x) - exp(x);
+}
+
+float df(float x){
+    return 4*cos(x) - exp(x);
+}
 
 int main(){
     float x0, x1, error;
-    float f0, f1, df0, df1;
     int N, steps;
-    steps = 0;
+    steps = 1;
     printf("Enter initial value: ");
     scanf("%f", &x0);
 
@@ -19,19 +23,16 @@ int main(){
     printf("Enter number of iterations: ");
     scanf("%d", &N);
 
-    printf("\nStep\t\tx0\t\tx1\t\tfx0\t\tfx1\t\tdfx0\t\tdfx1\n");
+    printf("\nStep\t\tx0\t\tf(x0)\t\tdf(x0)\t\tx1\t\tf(x1)\n");
 
     do{
-        df0 = df(x0);
-        if(df0 == 0){
+        if(df(x0) == 0){
             printf("Math error! Choose another initial guess.");
             exit(1);
         }
 
-        f0 = f(x0);
-
-        x1 = x0 - (f0)/(df0);
-        printf("\n%d\t%f\t%f  \t%f\t\t%f  \t%f\t%f\n", steps, x0, x1, f(x0), f(x1), df(x0), df(x1));
+        x1 = x0 - f(x0)/df(x0);
+        printf("\n%d\t\t%f\t%f\t%f\t%f\t%f\n", steps, x0, f(x0), df(x0), x1, f(x1));
 
         if(steps > N){
             printf("Not convergent!");
@@ -40,8 +41,7 @@ int main(){
         steps++;
 
         x0 = x1;
-        f1 = f(x1);
-    }while( fabs(f1) > error);
+    }while( fabs(f(x1)) > error);
 
     printf("\nRoot: %f", x1);
 
